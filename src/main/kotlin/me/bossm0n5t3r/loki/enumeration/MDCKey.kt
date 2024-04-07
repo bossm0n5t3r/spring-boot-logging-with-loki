24ba9e7891080f597157d79aa7a13cb3112d9711
bossm0n5t3r <bossm0n5t3r@gmail.com>
2024-04-08 01:00:00 +0900

    feat: Add MDCKey

---
+++ src/main/kotlin/me/bossm0n5t3r/loki/enumeration/MDCKey.kt
@@ -0,0 +1,8 @@
+package me.bossm0n5t3r.loki.enumeration
+
+enum class MDCKey(val value: String) {
+    TRANSACTION_ID("transactionId"),
+    SERVICE_CODE("serviceCode"),
+    TASK_CODE("taskCode"),
+    USER_ID("userId"),
+}
